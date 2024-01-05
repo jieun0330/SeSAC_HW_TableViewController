bae7c7cdbf41b95aa425f9402bc6f19c53b9ed7f
jieun <jieun_0330@naver.com>
2024-01-05 19:04:52 +0900

    first commit

---
+++ SeSAC_HW_TableViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_HW_TableViewController
+//
+//  Created by 박지은 on 1/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
