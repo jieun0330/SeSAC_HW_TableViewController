4ba4033799acb0d0471414ed117b418bbcec17c1
jieun <jieun_0330@naver.com>
2024-01-06 16:50:47 +0900

    디자인 영역 함수로 빼기

--- SeSAC_HW_TableViewController/ShoppingTableViewController.swift
+++ SeSAC_HW_TableViewController/ShoppingTableViewController.swift
@@ -17,42 +17,55 @@ class ShoppingTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        shoppingTextField.placeholder = "무엇을 구매하실 건가요?"
-        // 아래 패딩주기
-        // 섹션마다 간격 주기
-        shoppingTextField.backgroundColor = .systemGray6
-        shoppingTextField.layer.cornerRadius = 10
-        shoppingTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
-        shoppingTextField.leftViewMode = .always
-        addButton.setTitle("추가", for: .normal)
-        addButton.backgroundColor = .systemGray5
-        addButton.layer.cornerRadius = 5
-        addButton.setTitleColor(.black, for: .normal)
+        textField(shoppingTextField)
+        add(addButton)
     }
     
+    //Mark: - 텍스트필드 디자인
+    func textField(_ sender: UITextField) {
+        sender.placeholder = "무엇을 구매하실 건가요?"
+        // 아래 패딩주기
+        // 섹션마다 간격 주기
+        sender.backgroundColor = .systemGray6
+        sender.layer.cornerRadius = 10
+        sender.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
+        sender.leftViewMode = .always
+    }
+    
+    //Mark: - 아이템 추가 버튼 디자인
+    func add(_ sender: UIButton) {
+        sender.setTitle("추가", for: .normal)
+        sender.backgroundColor = .systemGray5
+        sender.layer.cornerRadius = 5
+        sender.setTitleColor(.black, for: .normal)
+    }
+    
+    //Mark: - 아이템 추가 버튼 기능
     @IBAction func itemAdded(_ sender: UIButton) {
         list.append(shoppingTextField.text!)
         tableView.reloadData()
     }
     
+    //Mark: - check 버튼
     // 다시 누르면 체크 안된 상태로 돌아가기
     @IBAction func checkButtonClicked(_ sender: UIButton) {
         sender.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
     }
     
+    //Mark: - 찜 버튼
     // 다시 누르면 체크 안된 상태로 돌아가기
     @IBAction func likeButtonClicked(_ sender: UIButton) {
         sender.setImage(UIImage(systemName: "star.fill"), for: .normal)
     }
     
     
-    // 1. 셀 갯수
+    //Mark: - 1. 셀 갯수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return list.count
     }
     
-    // 2. 셀 디자인 및 데이터 처리
+    //Mark: - 2. 셀 디자인 및 데이터 처리
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingTableViewCell", for: indexPath) as! ShoppingTableViewCell
@@ -69,12 +82,13 @@ class ShoppingTableViewController: UITableViewController {
         return cell
     }
     
-    // 3. 셀 높이
+    //Mark: - 3. 셀 높이
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
         return 44
     }
     
+    //Mark: - 셀 삭제 기능
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
