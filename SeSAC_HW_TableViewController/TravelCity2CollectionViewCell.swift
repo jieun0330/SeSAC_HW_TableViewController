f72aab26dc4c1da8940d566001bf75b7398c7f87
jieun <jieun_0330@naver.com>
2024-01-09 18:44:52 +0900

    세그먼트 없이, 컬렉션뷰컨트롤러와 XIB 셀로 구현하기

--- SeSAC_HW_TableViewController/TravelCity2CollectionViewCell.swift
+++ SeSAC_HW_TableViewController/TravelCity2CollectionViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  TravelCityCollectionViewCell.swift
+//  TravelCity2CollectionViewCell.swift
 //  SeSAC_HW_TableViewController
 //
 //  Created by 박지은 on 1/9/24.
@@ -7,19 +7,19 @@
 
 import UIKit
 
-class TravelCityCollectionViewCell: UICollectionViewCell {
+class TravelCity2CollectionViewCell: UICollectionViewCell {
     
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var cityLabel: UILabel!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         
         imageView.contentMode = .scaleAspectFill
         imageView.layer.masksToBounds = false
-        imageView.layer.cornerRadius = imageView.frame.height / 2
-//        print(imageView.frame.height) // 내가 140으로 줌
+        imageView.layer.cornerRadius = imageView.frame.height / 3.7
+//        print(imageView.frame.height) // 동그라미 만드는 과정을 숫자를 다 찍어봐야되는건가?.?
         imageView.clipsToBounds = true
         
         titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
@@ -28,8 +28,7 @@ class TravelCityCollectionViewCell: UICollectionViewCell {
         cityLabel.textColor = .systemGray3
         cityLabel.numberOfLines = 2
         cityLabel.textAlignment = .center
-        
+//        
     }
-    
-    
+
 }
---
+++ SeSAC_HW_TableViewController/TravelCity2CollectionViewController.swift
@@ -0,0 +1,53 @@
+//
+//  TravelCityCollectionViewController2.swift
+//  SeSAC_HW_TableViewController
+//
+//  Created by 박지은 on 1/9/24.
+//
+
+import UIKit
+import Kingfisher
+
+private let reuseIdentifier = "Cell"
+
+class TravelCity2CollectionViewController: UICollectionViewController {
+    
+    let travel = CityInfo()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let xib = UINib(nibName: "TravelCity2CollectionViewCell", bundle: nil)
+        collectionView.register(xib, forCellWithReuseIdentifier: "TravelCity2CollectionViewCell")
+        
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 24
+        let cellWidth = UIScreen.main.bounds.width - (spacing * 3)
+        
+        layout.itemSize = CGSize(width: cellWidth / 2, height: cellWidth / 1.6)
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        
+        collectionView.collectionViewLayout = layout
+        
+    }
+    
+    // 1. 셀 높이
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return travel.city.count
+    }
+    
+    // 2. 셀 디자인 및 데이터 처리
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TravelCity2CollectionViewCell", for: indexPath) as! TravelCity2CollectionViewCell
+        
+        cell.imageView.kf.setImage(with: URL(string: travel.city[indexPath.row].city_image))
+        cell.titleLabel.text = "\(travel.city[indexPath.row].city_name) | \(travel.city[indexPath.row].city_english_name)"
+        cell.cityLabel.text = travel.city[indexPath.row].city_explain
+        
+        return cell
+    }
+    
+}
