c2f5880ef7a0069acd91a38709b010b174231064
jieun <jieun_0330@naver.com>
2024-01-10 22:39:42 +0900

    protocol, extension 일단 냅다 해보기,,

--- SeSAC_HW_TableViewController/TravelCity3CollectionViewCell.swift
+++ SeSAC_HW_TableViewController/TravelCity3CollectionViewCell.swift
@@ -16,12 +16,13 @@ class TravelCity3CollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        configureUI()
+    }
+    
+    func configureUI() {
         imageView.contentMode = .scaleAspectFill
         imageView.layer.masksToBounds = false
         imageView.layer.cornerRadius = imageView.frame.height / 4
-        print(imageView.frame.height) // 동그라미 만드는 과정을 숫자를 다 찍어봐야되는건가?.?
-        // frame.height에 따라 유동적으로 계속 바뀌니까 고정적인 숫자를 줘야되는건가
-        // 머리~ 안도라가~
         imageView.clipsToBounds = true
         
         titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
@@ -33,3 +34,5 @@ class TravelCity3CollectionViewCell: UICollectionViewCell {
         cityLabel.textAlignment = .center
     }
 }
+
+
--- SeSAC_HW_TableViewController/TravelCity3ViewController.swift
+++ SeSAC_HW_TableViewController/TravelCity3ViewController.swift
@@ -14,7 +14,47 @@ enum domestic: String, CaseIterable {
     case overseas = "해외"
 }
 
-class TravelCity3ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+protocol XibRegisterable {
+    var identifier: String { get set }
+    func register(identifier: String)
+}
+
+class TravelCity3ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, XibRegisterable {
+    
+    //Mark: - XibRegisterable
+    var identifier: String = "TravelCity3CollectionViewCell"
+    func register(identifier: String) {
+        let xib2 = UINib(nibName: identifier, bundle: nil)
+        cityView.register(xib2, forCellWithReuseIdentifier: identifier)
+    }
+    
+    @IBOutlet var cityView: UICollectionView!
+    @IBOutlet var domesticSegment: UISegmentedControl!
+    
+    let travel = CityInfo()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        register(identifier: identifier)
+        configureCollectionView()
+        setLayout()
+    }
+}
+
+extension TravelCity3ViewController {
+    
+    func setupSegmentControl() {
+        domesticSegment.removeAllSegments()
+        domestic.allCases.enumerated().forEach{ (index, section) in domesticSegment.insertSegment(withTitle: section.rawValue, at: index, animated: false)}
+        domesticSegment.selectedSegmentIndex = 0
+    }
+    
+    func configureCollectionView() {
+        cityView.delegate = self
+        cityView.dataSource = self
+        
+    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return travel.city.count
@@ -30,20 +70,7 @@ class TravelCity3ViewController: UIViewController, UICollectionViewDelegate, UIC
         return cell
     }
     
-    @IBOutlet var cityView: UICollectionView!
-    @IBOutlet var domesticSegment: UISegmentedControl!
-    
-    let travel = CityInfo()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let xib2 = UINib(nibName: "TravelCity3CollectionViewCell", bundle: nil)
-        cityView.register(xib2, forCellWithReuseIdentifier: "TravelCity3CollectionViewCell")
-        
-        cityView.delegate = self
-        cityView.dataSource = self
-        
+    func setLayout() {
         let layout = UICollectionViewFlowLayout()
         let spacing: CGFloat = 24
         let cellWidth = UIScreen.main.bounds.width - (spacing * 3)
@@ -52,15 +79,6 @@ class TravelCity3ViewController: UIViewController, UICollectionViewDelegate, UIC
         layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
         layout.minimumLineSpacing = spacing
         layout.minimumInteritemSpacing = spacing
-        
         cityView.collectionViewLayout = layout
-        
-        setupSegmentControl()
-    }
-    
-    private func setupSegmentControl() {
-        domesticSegment.removeAllSegments()
-        domestic.allCases.enumerated().forEach{ (index, section) in domesticSegment.insertSegment(withTitle: section.rawValue, at: index, animated: false)}
-        domesticSegment.selectedSegmentIndex = 0
     }
 }
